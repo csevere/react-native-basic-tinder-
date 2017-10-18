98dd8bd5409805ded4d46fe5758c8baeaf895efe
csevere <carla.severe@gmail.com>
2017-10-18 14:24:42 -0400

    rendered text for cards

--- App.js
+++ App.js
@@ -3,36 +3,46 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
 import reducers from './src/reducers'; 
-import config from './config/config'; 
-import firebase from 'firebase'; 
+import Deck from './src/Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 
 class App extends Component{
-  componentWillMount(){
-    var connection = {
-      apiKey: config.apiKey,
-      authDomain: config.authDomain,
-      databaseURL: config.databaseURL,
-      projectId: config.projectId,
-      storageBucket: config.storageBucket,
-      messagingSenderId: config.messagingSenderId
-    };
-    firebase.initializeApp(connection);
-
   
+  renderCard(item) {
+    return(
+      <Text>{item.text}</Text>
+    )
   }
+  
   render(){
-    return (
-      <Provider store = {createStore(reducers)}>
-        <View>
-          <Text>Hello</Text> 
-        </View>
-      </Provider> 
-  
+    return(
+      <View style = {styles.container}>
+        <Deck 
+          data = {DATA}
+          renderCard = {this.renderCard}
+        />
+      </View>
     );
-
   }
-
 };
+
+const styles = {
+  container:{
+    flex: 1,
+    backgroundColor: '#fff',
+      
+  }
+}; 
 
 export default App; 
